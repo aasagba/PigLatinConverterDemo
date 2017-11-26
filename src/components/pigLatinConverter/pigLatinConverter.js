494f356010c2447f1085130fd107379081702136
adrianasagba <aasagba@andigital.com>
2017-11-26 14:57:23 +0000

    get user input and check if consonant or vowel

--- src/components/pigLatinConverter/pigLatinConverter.js
+++ src/components/pigLatinConverter/pigLatinConverter.js
@@ -9,20 +9,44 @@ class pigLatinConverterController {
     $onInit() {
         this.title = 'test title';
         this.input = '';
-        this.words = [];
+        this.output = '';
         this.history = [];
         this.vowels = ['a', 'e', 'i', 'o', 'u'];
     }
 
-    getInput (input) {
+    doConversion () {
+        let words = this.checkInput();
 
+        // loop through array of words and check 1st letters
+        words.forEach((word) => {
+            let l = word.slice(0,1);
+            // check pattern
+            let pattern = this.checkLetterPattern(l);
+            pattern === 'vowel' ? this.doVowelConversion(pattern) : this.doConsonantConversion(pattern);
+        });
     }
 
-    checkLetterPattern () {
-
+    checkInput () {
+        // get words and split into array of words
+        return this.input.split(' ');
     }
 
-    convertToPigLatin () {
+    checkLetterPattern (letter) {
+        letter = letter.toLowerCase();
+        let pattern = this.vowels.includes(letter) ? 'vowel' : 'consonant';
+
+        return pattern;
+    }
+
+    doVowelConversion(pattern) {
+        console.log(`doing ${pattern} conversion`);
+    }
+
+    doConsonantConversion(pattern) {
+        console.log(`doing ${pattern} conversion`);
+    }
+
+    convertToPigLatin (pattern) {
 
     }
 
