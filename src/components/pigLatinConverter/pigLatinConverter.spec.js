08c9b0961f1e6083d8b8e34a9a289b0d05cec7bf
adrianasagba <aasagba@andigital.com>
2017-11-26 12:01:12 +0000

    Add pig Latin Converter tests

--- src/components/pigLatinConverter/pigLatinConverter.spec.js
+++ src/components/pigLatinConverter/pigLatinConverter.spec.js
@@ -1,5 +1,33 @@
-describe('example test', function() {
-    it('should be true', function() {
-        expect('foo').toBe('foo');
+import pigLatinConverter from './pigLatinConverter';
+
+describe('Pig Latin Converter ', () => {
+
+    let $ctrl, $scope, $componentController;
+
+    beforeEach(() => {
+        angular.mock.module(pigLatinConverter);
+        inject((_$componentController_, $rootScope) => {
+            $componentController = _$componentController_;
+            $scope = $rootScope.$new();
+        });
+        $ctrl = $componentController('pigLatinConverterController', $scope, {
+        });
     });
+
+    it('it should accept words', () => {
+
+    });
+
+    it('it should convert the words into Pig Latin', () => {
+
+    });
+
+    it('it should output the Pig Latin version of the Word', () => {
+
+    });
+
+    it('it should Keep a track of the history of the last 10 words/sentences and also display these', () => {
+
+    });
+
 });
